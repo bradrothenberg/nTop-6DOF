d02e3eacc56ebd02d986a1f8ac09cc096b0c54f6
Brad Rothenberg <bradrothenberg@gmail.com>
2025-11-10 15:11:30 -0500

    Clean up old/intermediate files and add test outputs

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -4,3 +4,32 @@ Core 6-DOF flight dynamics components.
 This module provides the fundamental building blocks for six-degree-of-freedom
 aircraft flight simulation.
 """
+
+from .aerodynamics import (
+    AeroModel,
+    ConstantCoeffModel,
+    LinearAeroModel,
+    FlyingWingAeroModel,
+    AVLTableModel
+)
+from .dynamics import AircraftDynamics
+from .state import State
+from .quaternion import Quaternion
+from .integrator import RK4Integrator, RK45Integrator
+from .propulsion import ConstantThrustModel, PropellerModel, CombinedForceModel
+
+__all__ = [
+    'AeroModel',
+    'ConstantCoeffModel',
+    'LinearAeroModel',
+    'FlyingWingAeroModel',
+    'AVLTableModel',
+    'AircraftDynamics',
+    'State',
+    'Quaternion',
+    'RK4Integrator',
+    'RK45Integrator',
+    'ConstantThrustModel',
+    'PropellerModel',
+    'CombinedForceModel'
+]
